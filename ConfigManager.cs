2977e77a59d072c6f0b026bc80f4d5bdbf60353f
Justalazydev <franklittlealijr@gmail.com>
2025-03-10 21:11:08 -0500

    Update ConfigManager.cs
    Update Added functions!

--- ConfigManager.cs
+++ ConfigManager.cs
@@ -2,12 +2,12 @@ using REPOConfig;
 
 public static class ConfigManager
 {
+
     public static bool IsKickEnabled { get; private set; }
     public static bool RequireConfirmation { get; private set; }
 
     public static void LoadConfig()
     {
-        
         IsKickEnabled = REPOConfigManager.GetBool("KickEnabled", true);
         RequireConfirmation = REPOConfigManager.GetBool("RequireConfirmation", true);
     }
