c2c135ebd07cc24db07ac4025a0ebbddcb15ee7b
Justalazydev <franklittlealijr@gmail.com>
2025-03-10 21:10:44 -0500

    Update RepoKickMod.cs
    Update Added functions!

--- RepoKickMod.cs
+++ RepoKickMod.cs
@@ -1,14 +1,39 @@
 using BepInEx;
 using HarmonyLib;
 using REPOConfig;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-[BepInPlugin("com.TankDaDank.RepKickMod", "R.E.P.0 Kick Mod", "1.3.0")]
+[BepInPlugin("com.TankDaDank.REPOKickMod", "REPO Kick Mod", "1.3.0")]
 public class REPOKickMod : BaseUnityPlugin
 {
     private void Awake()
     {
         ConfigManager.LoadConfig();
-        Harmony harmony = new Harmony(com.TankDaDank.RepKickMod");
+
+        Harmony harmony = new Harmony("com.TankDaDank.REPOKickMod");
         harmony.PatchAll();
+        
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "MultiplayerLobby")
+        {
+            if (KickManager.IsHost() && ConfigManager.IsKickEnabled)
+            {
+                UIManager.InitializeUI();
+            }
+        }
+        else
+           
+            UIManager.ClearUI();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
