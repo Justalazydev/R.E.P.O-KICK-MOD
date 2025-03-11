b6aea282370c8ada5583067c16b04a2144a9365d
Justalazydev <franklittlealijr@gmail.com>
2025-03-10 21:07:19 -0500

    Create ConfigManager.cs

---
+++ ConfigManager.cs
@@ -0,0 +1,14 @@
+using REPOConfig;
+
+public static class ConfigManager
+{
+    public static bool IsKickEnabled { get; private set; }
+    public static bool RequireConfirmation { get; private set; }
+
+    public static void LoadConfig()
+    {
+        
+        IsKickEnabled = REPOConfigManager.GetBool("KickEnabled", true);
+        RequireConfirmation = REPOConfigManager.GetBool("RequireConfirmation", true);
+    }
+}
